a6162af32f89c923ecbfe8ca2eeed2c1290342ac
Danilo <124076190+DaniloLMD@users.noreply.github.com>
2024-06-24 18:30:08 +0000

    acho que vou formar desempregado

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//comando pra rodar = npm run dev
+
+import { Header } from "./components/Header"
+
+function App() {
+
+  return (
+    <div> 
+      <Header title={"POO"}/>
+      <Header title={"E-commerce"}/>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </div>
+  )
+}
+
+export default App
